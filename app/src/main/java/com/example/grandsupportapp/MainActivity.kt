54958640b1a04d4c8e1e83ddd4d499bb485082c6
Ilya Ponomaryov <ip287733@gmail.com>
2020-01-23 23:54:27 +0500

    first

--- app/src/main/java/com/example/grandsupportapp/MainActivity.kt
+++ app/src/main/java/com/example/grandsupportapp/MainActivity.kt
@@ -2,16 +2,15 @@ package com.example.grandsupportapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val lv = findViewById<ListView>(R.id.lv_menu)
-        
+
 
     }
 }
