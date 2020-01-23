06567d19c3031edd9f1cd47034c60afea416dbb5
rep <makskhodzhabekov@gmail.com>
2020-01-23 15:13:26 +0500

    Initial commit

---
+++ app/src/main/java/com/example/grandsupportapp/adapters/MenuLvAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.grandsupportapp.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.grandsupportapp.R
+
+class MenuLvAdapter(private val context: Context, private val menu: MutableList<String>): BaseAdapter() {
+
+    private val inflater: LayoutInflater =
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val rowView = inflater.inflate(R.layout.item_lv_main, parent, false)
+        val nameText = rowView.findViewById<TextView>(R.id.item_menu_name)
+        //nameText.text =
+        return rowView
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/com/example/grandsupportapp/models/MenuItem.kt
@@ -0,0 +1,10 @@
+package com.example.grandsupportapp.models
+
+class MenuItem {
+
+        private val menu = mutableListOf<String>(
+            "Смартфон - телефон", "Госуслуги", "Запись к врачу", "Соц защита", "ЖКХ",
+            "Мобильный банк"
+        )
+
+}
