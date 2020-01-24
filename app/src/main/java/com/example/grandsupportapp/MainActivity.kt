df08cf8b7e83996c6a8be8b5e401b5c595a2bd6c
rep <makskhodzhabekov@gmail.com>
2020-01-24 15:17:11 +0500

    2

--- app/src/main/java/com/example/grandsupportapp/MainActivity.kt
+++ app/src/main/java/com/example/grandsupportapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.grandsupportapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -10,7 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val smartphoneBtn = findViewById<Button>(R.id.smartphone_btn)
+        val regBtn = findViewById<Button>(R.id.reg_btn)
+        val doctorBtn = findViewById<Button>(R.id.doctor_btn)
+        val socBtn = findViewById<Button>(R.id.soc_btn)
+        val zhkhBtn = findViewById<Button>(R.id.zhkh_btn)
+        val mobileBankBtn = findViewById<Button>(R.id.mob_bank_btn)
 
+        smartphoneBtn.setOnClickListener {
+            //startActivity(Intent())
+        }
 
     }
 }
