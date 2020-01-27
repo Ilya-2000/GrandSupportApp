f90987478435e6871be145e1927636e44608fe3a
rep <makskhodzhabekov@gmail.com>
2020-01-27 16:17:23 +0500

    creating menu

--- app/src/main/java/com/example/grandsupportapp/MainActivity.kt
+++ app/src/main/java/com/example/grandsupportapp/MainActivity.kt
@@ -12,16 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val smartphoneBtn = findViewById<Button>(R.id.smartphone_btn)
-        val regBtn = findViewById<Button>(R.id.reg_btn)
-        val doctorBtn = findViewById<Button>(R.id.doctor_btn)
-        val socBtn = findViewById<Button>(R.id.soc_btn)
-        val zhkhBtn = findViewById<Button>(R.id.zhkh_btn)
-        val mobileBankBtn = findViewById<Button>(R.id.mob_bank_btn)
-
-        smartphoneBtn.setOnClickListener {
-            //startActivity(Intent())
-        }
 
     }
 }
