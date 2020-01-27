0c283079f3b779e57cdf1960e30cf349e9915f93
Ilya Ponomaryov <ip287733@gmail.com>
2020-01-28 00:41:11 +0500

    initialize menu btn

--- app/src/main/java/com/example/grandsupportapp/MainActivity.kt
+++ app/src/main/java/com/example/grandsupportapp/MainActivity.kt
@@ -6,12 +6,41 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import androidx.cardview.widget.CardView
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val smartphoneBtn: CardView = findViewById(R.id.smartphone_menu_btn)
+        val regBtn: CardView = findViewById(R.id.reg_menu_btn)
+        val healthBtn: CardView = findViewById(R.id.health_menu_btn)
+        val socBtn: CardView = findViewById(R.id.social_menu_btn)
+        val communalBtn: CardView = findViewById(R.id.communal_menu_btn)
+        val bankBtn: CardView = findViewById(R.id.mobile_bank_menu_btn)
+
+
+        smartphoneBtn.setOnClickListener {
+            startActivity(Intent(this, SmartphoneActivity::class.java))
+        }
+        regBtn.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
+        healthBtn.setOnClickListener {
+
+        }
+        socBtn.setOnClickListener {
+
+        }
+        communalBtn.setOnClickListener {
+
+        }
+        bankBtn.setOnClickListener {
+
+        }
+
+
 
     }
 }
