29dff9c3bbd2f047cf0adb513c9597544e970601
rep <makskhodzhabekov@gmail.com>
2020-01-30 14:47:08 +0500

    creating fragments

---
+++ app/src/main/java/com/example/grandsupportapp/registration_steps/SevenRegFragment.kt
@@ -0,0 +1,26 @@
+package com.example.grandsupportapp.registration_steps
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.grandsupportapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SevenRegFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_seven_reg, container, false)
+    }
+
+
+}
