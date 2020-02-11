dc2e1521f821d2b11185511e470eef004950f0d1
Ilya Ponomaryov <ip287733@gmail.com>
2020-02-11 23:19:54 +0500

    11.02 v1

---
+++ app/src/main/java/com/example/grandsupportapp/bank/money_transfer/TransferActivity.kt
@@ -0,0 +1,13 @@
+package com.example.grandsupportapp.bank.money_transfer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.grandsupportapp.R
+
+class TransferActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transfer)
+    }
+}
