0bc7218dc828732b5cca1e35bea1d0e241763436
rep <makskhodzhabekov@gmail.com>
2020-02-12 16:16:11 +0500

    12.02 v4

--- app/src/main/java/com/example/grandsupportapp/ContactsActivity.kt
+++ app/src/main/java/com/example/grandsupportapp/ContactsActivity.kt
@@ -8,5 +8,10 @@ class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.contact_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
     }
 }
--- app/src/main/java/com/example/grandsupportapp/bank/BankMainActivity.kt
+++ app/src/main/java/com/example/grandsupportapp/bank/BankMainActivity.kt
@@ -3,6 +3,7 @@ package com.example.grandsupportapp.bank
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.cardview.widget.CardView
 import com.example.grandsupportapp.R
 import com.example.grandsupportapp.bank.communal_payment.CommunalPaymentActivity
@@ -17,6 +18,10 @@ class BankMainActivity : AppCompatActivity() {
         val mobilePaymentBtn = findViewById<CardView>(R.id.mobile_payment_bank_btn)
         val communalPayBtn = findViewById<CardView>(R.id.communal_bank_btn)
         val transferPayBtn = findViewById<CardView>(R.id.card_bank_btn)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_bank_main)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
 
         mobilePaymentBtn.setOnClickListener {
             startActivity(Intent(this, MobilePaymentActivity::class.java))
