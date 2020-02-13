278a02ff39e88d509d1378aa14a8bbf78d946ced
rep <makskhodzhabekov@gmail.com>
2020-02-13 13:41:55 +0500

    13.02 v2

---
+++ app/src/main/java/com/example/grandsupportapp/social_network/FinalNetworkFragment.kt
@@ -0,0 +1,26 @@
+package com.example.grandsupportapp.social_network
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.grandsupportapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FinalNetworkFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_final_network, container, false)
+    }
+
+
+}
