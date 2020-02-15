01b8271e4c358c9b9e03c8b866887f1482cdb82f
Ilya Ponomaryov <ip287733@gmail.com>
2020-02-15 19:41:07 +0500

    15.02 v1

---
+++ app/src/main/java/com/example/grandsupportapp/smartphone/calls/FinalCallFragment.kt
@@ -0,0 +1,26 @@
+package com.example.grandsupportapp.smartphone.calls
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.grandsupportapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FinalCallFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_final_call, container, false)
+    }
+
+
+}
